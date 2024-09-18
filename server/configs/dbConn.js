b5197c7911c622391e908d95332d72fdd9ab9149
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-09-18 19:49:43 +0530

    db

--- server/configs/dbConn.js
+++ server/configs/dbConn.js
@@ -1,12 +1,4 @@
 import mongoose from "mongoose";
-
-/**
- * mongoose.set("strictQuery", false); sets Mongoose's query strictness to false.
- * This means that Mongoose will not return an error if you try to query for a
- * non-existent field. Instead, it will simply return an empty result.
- * With strict query set to true, Mongoose will return an error if you try to
- * query for a non-existent field.
- */
 mongoose.set("strictQuery", false);
 
 const connectToDB = async () => {
