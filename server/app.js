70b2ce8e10c86ce68245414277f5c935ea2e7119
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-10-05 13:05:50 +0530

    new

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL] || 'https://lms-main-frontend-1.onrender.com',
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
   })
 );
