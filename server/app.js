0dac3193cfef0e8c2dc3e6bd2499574043de6e15
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-10-27 08:06:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ const corsOptions = {
   origin: [process.env.FRONTEND_URL] || "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 // Use CORS middleware
 app.use(cors(corsOptions));
