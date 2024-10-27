74b4b4b7e12880f3c8f7279cc4add94c41cb1574
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-10-27 08:26:59 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
