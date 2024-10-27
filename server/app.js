7c5766a4486d785a86230f37c1d56a3f20a3f9c1
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-10-27 07:50:38 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: [process.env.FRONTEND_URL] || "http://localhost:3000",
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 // Use CORS middleware
 app.use(cors(corsOptions));
