d6d3a0abcd4ce8750e7a4e148ca6d5ff3a1b48bf
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2024-10-27 08:58:21 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,10 +4,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import errorMiddleware from './middlewares/error.middleware.js';
-app.use(morgan('dev'));
 config();
 const app = express();
-
+app.use(morgan('dev'));
 // Middleware Setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
